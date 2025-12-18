3acba852b71189975052ad24717fd21bb77bd772
Enes004 <enessatici20@gmail.com>
2025-12-18 17:23:56 +0300

    pointers added

--- pointers.cpp
+++ pointers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 int main(){
     int x=3;
@@ -6,6 +7,22 @@ int main(){
     xptr = &x;
     std::cout << *xptr << std::endl;
 
-
+    const int *a = &x; //cant change the value
+    int const *b = &x; // cant change address
+    const int const &x; // cant change anything
     return 0;
+
+    //Reference
+    int x = 10;
+    int& r = x;
+    /*
+    Reference adres tutmaz,
+    Reference başka bir değişken değildir,
+    Reference kopya değildir.
+    Hafızada yeni bir int yok.
+    r diye ayrı bir kutu yok.
+    Tek kutu var, iki isim var.
+    “Reference pointer gibi erişim sağlar”
+    “Ama pointer gibi nesne değildir”
+    */
 }
