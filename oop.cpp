86283734e7606da8340e3cb588881797c72812bc
Enes004 <enessatici20@gmail.com>
2025-12-19 03:07:02 +0300

    polymorphism added

--- oop.cpp
+++ oop.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 class BattleBot {
-    private:
+    protected:
     std::string name;
     int hp;
 
@@ -14,17 +14,23 @@ class BattleBot {
         std::cout << name << "fabrikadan cikti" <<std::endl;
     }
 
-    ~BattleBot(){
+    virtual ~BattleBot(){
         std::cout << name << "Hurdaya cikti"<<std::endl;
     }
     
-    void attack(){
+    virtual void attack(){
         std::cout << name << "Saldiriyor, Hp si : " << hp << std::endl;
 
     }
 
     int getHp(){
         return hp;
+    }
+
+    void setHp(int health){
+        if(health<0) hp = 0;
+        else if (health>100) hp = 100;
+        else hp=health;
     }
 
     void takeDamage(int damage){
@@ -36,11 +42,54 @@ class BattleBot {
 
 };
 
+class TankBot : public BattleBot{
 
-int main(){
-    BattleBot bot1("Matrix",100);
-    bot1.attack();
-    std::cout << "Mecvut can : "<<bot1.getHp()<<std::endl;
+    private:
+    int armor;
+
+    public:
+
+    //Constructor
+    TankBot(std::string n , int h , int a) : BattleBot(n,h){
+        armor = a;
+        std::cout << "Tankbot "<<armor<< "u kusandi" << std::endl;
+    }
+
+
+    void defense(){
+        std::cout <<name<< " tarafindan zirh giyildi defans guclu "<<std::endl;
+    }
+
+    //override
+    void attack(){
+        std::cout <<name<< " top atişi yapiyorr"<<std::endl;
+    }
+
+    ~TankBot(){
+        std::cout <<name<< "hurdaya cikti" <<std::endl;
+    }
+
+
+
+
+
+};
+
+
+
+
+int main() {
+    TankBot tank1("Panzer", 200, 50);
+
+    // POLYMORPHISM TESTİ
+    // Tankı, genel bir Robot pointer'ı ile tutuyoruz.
+    BattleBot* genelRobot = &tank1;
+
+    std::cout << "\n--- POINTER UZERINDEN SALDIRI ---" << std::endl;
+    
+    // Eğer 'virtual' yazdıysan "Top Atisi" yazar.
+    // Yazmadıysan "Standart Saldiri" yazar.
+    genelRobot->attack(); 
 
     return 0;
 }
